da113d962be15bee23e01ba4a3f0b4af80324d8e
Ayodejioladimeji <layobright11@gmail.com>
2022-10-01 10:40:27 +0100

    completed the negotiate component

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 
 // import logo
-import logo from '../../assets/logo.png';
-import { Link } from 'react-scroll';
-import './Navbar.css';
-import Button from '../../common/button/Button';
+import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
+import "./Navbar.css";
+import Button from "../../common/button/Button";
 
 //
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  // Changing the navbar color on scroll
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
   return (
-    <div className='nav'>
-      <div className='nav-brand'>
-        <img src={logo} alt='brand-logo' />
+    <div className={navbar ? "active-nav nav" : "nav"}>
+      <div className="nav-brand">
+        <img src={logo} alt="brand-logo" />
       </div>
 
-      <div className='nav-button'>
+      <div className="nav-button">
         <Link
-          to='newsletter'
+          to="newsletter"
           spy={true}
           smooth={true}
           offset={-70}
           duration={500}
         >
-          <Button text='Join waitlist' background='var(--primary-color)' />
+          <Button text="Join waitlist" background="var(--primary-color)" />
         </Link>
       </div>
     </div>

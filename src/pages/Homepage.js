a3d168328b776272ec7b6a360c3c7cb95fe839c5
Ayodejioladimeji <layobright11@gmail.com>
2022-12-16 17:45:25 +0100

    completed section one responsiveness

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import Header from './../components/nav/Header';
-import HeroPage from './../components/hero/HeroPage';
-import Scan from './../components/scan/Scan';
-import WhatWeDo from '../components/whatwedo/WhatWeDo';
-import Negotiate from '../components/negotiate/Negotiate';
-import Enjoy from '../components/enjoy/Enjoy';
-import SectionOne from '../components/section-one/SectionOne';
-import SectionTwo from '../components/section-two/SectionTwo';
-import SectionThree from '../components/section-three/SectionThree';
-import SEO from './../SEO/Seo';
-import Rent from '../components/rent/Rent';
-import Footer from '../components/footer/Footer';
+import React from "react";
+import Header from "./../components/nav/Header";
+import HeroPage from "./../components/hero/HeroPage";
+import Scan from "./../components/scan/Scan";
+import WhatWeDo from "../components/whatwedo/WhatWeDo";
+import Negotiate from "../components/negotiate/Negotiate";
+import Enjoy from "../components/enjoy/Enjoy";
+import SectionOne from "../components/section-one/SectionOne";
+import SectionTwo from "../components/section-two/SectionTwo";
+import SectionThree from "../components/section-three/SectionThree";
+import SEO from "./../SEO/Seo";
+import Rent from "../components/rent/Rent";
+import Footer from "../components/footer/Footer";
 
 //
 const Homepage = () => {
   return (
     <>
-      <SEO title='Homepage' />
+      <SEO title="Homepage" />
       <Header />
       <HeroPage />
       <SectionOne />
-      <SectionTwo />
-      <SectionThree />
-      <Enjoy />
-      <Scan />
-      <WhatWeDo />
-      <Negotiate />
-      <Rent />
-      <Footer />
+      {/* <SectionTwo /> */}
+      {/* <SectionThree /> */}
+      {/* <Enjoy /> */}
+      {/* <Scan /> */}
+      {/* <WhatWeDo /> */}
+      {/* <Negotiate /> */}
+      {/* <Rent /> */}
+      {/* <Footer /> */}
     </>
   );
 };
